5c292c0817c84d8a2c1ebd2c0ef874e03d7760a8
Ann Tseng <anntseng@Anns-MacBook-Pro.local>
2023-07-15 22:20:42 -0700

    working on date display functions

--- reactcalendar/src/Components/Calendar/Calendar.js
+++ reactcalendar/src/Components/Calendar/Calendar.js
@@ -2,35 +2,56 @@ import React from 'react';
 import '../Stylings/Calendar.css';
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import {getYear, getMonth, monDisplay} from '../Calendar/DateFunctions.js';
 
-export function Calendar() {
-    // var x = 0;
+class Calendar extends React.Component {
+    render() {
+        var year = getYear();
+        var month = getMonth();
 
-    // function addOne() {
-    //     x = x + 1;
-    //     console.log(x);
-    // }
+        return (
+            <div className="calendarContainer">
+                <div className="calendarHeader">
+                    <h1>{month} {year}</h1>
+                    <button className="arrowBack"> <ArrowBackIosNewIcon /> </button>
+                    <button className="arrowForward"> <ArrowForwardIosIcon /> </button>
+                </div>
 
-    return (
-        <div className="calendarContainer">
-            <div className="calendarHeader">
-                <h1>July 2023</h1>
-                <button className="arrowBack" /*onClick={addOne}*/> <ArrowBackIosNewIcon /> </button>
-                <button className="arrowForward"> <ArrowForwardIosIcon /> </button>
+                <div className="daysDisplay">
+                    <div className="weekDayDiv" id="day1Div">
+                        <h5>Mon</h5>
+                        {monDisplay()}
+                        {/* <p>15</p><p>15</p><p>15</p><p>15</p><p>15</p><p>15</p><p>15</p> */}
+                    </div>
+                    <div className="weekDayDiv" id="day2Div">
+                        <h5>Tue</h5>
+                        {/* <p>15</p><p>15</p><p>15</p><p>15</p><p>15</p><p>15</p><p>15</p> */}
+                    </div>
+                    <div className="weekDayDiv" id="day3Div">
+                        <h5>Wed</h5>
+                        {/* <p>15</p><p>15</p><p>15</p><p>15</p><p>15</p><p>15</p><p>15</p> */}
+                    </div>
+                    <div className="weekDayDiv" id="day4Div">
+                        <h5>Thu</h5>
+                        {/* <p>15</p><p>15</p><p>15</p><p>15</p><p>15</p><p>15</p><p>15</p> */}
+                    </div>
+                    <div className="weekDayDiv" id="day5Div">
+                        <h5>Fri</h5>
+                        {/* <p>15</p><p>15</p><p>15</p><p>15</p><p>15</p><p>15</p><p>15</p> */}
+                    </div>
+                    <div className="weekDayDiv" id="day6Div">
+                        <h5>Sat</h5>
+                        {/* <p>15</p><p>15</p><p>15</p><p>15</p><p>15</p><p>15</p><p>15</p> */}
+                    </div>
+                    <div className="weekDayDiv" id="day7Div">
+                        <h5>Sun</h5>
+                        {/* <p>15</p><p>15</p><p>15</p><p>15</p><p>15</p><p>15</p><p>15</p> */}
+                    </div>
+                </div>
             </div>
-
-            <div className="weekHeader">
-                <h5>Mon</h5>
-                <h5>Tue</h5>
-                <h5>Wed</h5>
-                <h5>Thu</h5>
-                <h5>Fri</h5>
-                <h5>Sat</h5>
-                <h5>Sun</h5>
-            </div>
-
-            
-
-        </div>
-    );
+        );
+    }
 }
+
+export {Calendar};
+
---
+++ reactcalendar/src/Components/Calendar/DateFunctions.js
@@ -0,0 +1,151 @@
+const theDate = new Date();
+
+export function getYear() {
+    var year = theDate.getFullYear();
+    return year;
+}
+
+export function getMonth() {
+    var monthNum = getMonthNum();
+    if(monthNum === 1) {
+        return "January";
+    } else if(monthNum === 2) {
+        return "February";
+    } else if(monthNum === 3) {
+        return "March";
+    } else if(monthNum === 4) {
+        return "April";
+    } else if(monthNum === 5) {
+        return "May";
+    } else if(monthNum === 6) {
+        return "June";
+    } else if(monthNum === 7) {
+        return "July";
+    } else if(monthNum === 8) {
+        return "August";
+    } else if(monthNum === 9) {
+        return "September";
+    } else if(monthNum === 10) {
+        return "October";
+    } else if(monthNum === 11) {
+        return "November";
+    }else {
+        return "December";
+    }
+}
+
+function isLeapYear(year) {
+    return ((year % 4 === 0) && (year % 100 !== 0)) || (year % 400 === 0);
+}
+
+function getMonthNum() {
+    var month = theDate.getMonth();
+    return month+1; //Prevent 0 being Jan and so on
+}
+
+export function getDay() {
+    var day = theDate.getDate();
+    var weekDay = theDate.toString().slice(0,3);
+    return [day, weekDay];
+}
+
+function findPairs(month, weekDayInt, year) {
+    var pairArray = [];
+    var pairIndex = 0;
+    var currentWeekDay = weekDayInt;
+    // console.log("weekDayInt: " + weekDayInt);
+    var monthLength = 0;
+
+    if(month === 2 && isLeapYear(year)) { //If February and leap year, has 29 days
+        monthLength = 29;
+    } else if(month === 2) { //Else February has 28 days
+        monthLength = 28;
+    } else {
+        if(month === 1 || month === 3 || month === 5 || month === 7 || month === 8 || month === 10 || month === 12) {
+            //Months with 31 days: January, March, May, July, August, October, and December
+            monthLength = 31;
+        } else {
+            //Months with 30 days: April, June, September, and November
+            monthLength = 30;
+        }
+    }
+    // console.log(monthLength);
+    for(var i = 1; i <= monthLength; i++) {
+        if(currentWeekDay > 7) {
+            currentWeekDay = 1;
+        }
+        var pair = [i, currentWeekDay];
+        pairArray[pairIndex] = pair;
+        pairIndex++;
+        currentWeekDay++;
+    }
+    return pairArray;
+}
+
+function getDaysArray(monthNum, yearNum) {
+    //Zeller's Rule:
+        //Zeller's rule explanation and source: https://www.themathdoctors.org/zellers-rule-what-day-of-the-week-is-it/
+        //Given the month, day, and year,
+        //we can use Zeller's rule to find the weekday for any given day of any month and year (leap year inclusive).
+        //We can find the weekday of the 1st day of a month, and understand how the other following days are to be layed-out in this calendar.
+        //Zeller's rule formula: N = (d + 2m + [3(m+1)/5] + y + [y/4] - [y/100] + [y/400] + 2)
+        //When we divide N by 7, and get the remainder, we can map it to the below code assigned to a particular weekday.
+            //1 = Sun, 2 = Mon, 3 = Tue, 4 = Wed, 5 = Thu, 6 = Fri, and 0 = Sat
+            // d = day, m = month, y = year
+            //The above formula's [] brackets represent flooring.
+        //NOTE: For months January and February, we need to use the numbers "13" and "14" correspondingly AND the previous year
+        //to complete the calculations correctly. E.g. If finding weekday of 1/1/98, then use 13/1/97 for the calculation.
+    var year = yearNum;
+    var month = monthNum;
+    var day = 1;
+
+    if(month === 1) {
+        year = year - 1;
+        month = 13;
+    } else if(month === 2) {
+        year = year - 1;
+        month = 14;
+    }
+    // console.log(month + " " + year);
+
+    var n1 = 2*month;
+    var n2 = Math.floor((3*(month+1))/5);
+    var n3 = Math.floor(year/4);
+    var n4 = Math.floor(year/100);
+    var n5 = Math.floor(year/400);
+
+    var n = day + n1 + n2 + year + n3 - n4 + n5 + 2;
+    var remainder = n % 7;
+    var weekDay = 0;
+
+    //1 = Sun, 2 = Mon, 3 = Tue, 4 = Wed, 5 = Thu, 6 = Fri, and 0 = Sat
+    if(remainder === 1) { //Sunday
+        weekDay = 7;
+    } else if(remainder === 2) { //Monday
+        weekDay = 1;
+    } else if(remainder === 3) { //Tuesday
+        weekDay = 2;
+    } else if(remainder === 4) { //Wednesday
+        weekDay = 3;
+    } else if(remainder === 5) { //Thursday
+        weekDay = 4;
+    } else if(remainder === 6) { //Friday
+        weekDay = 5;
+    } else { //remainder === 0) //Saturday
+        weekDay = 6;
+    }
+
+    //NOTE: we are using monthNum and yearNum here because it should be 1 = Jan and 2 = Feb
+    //while "month" and "year" variable above can equal 13, 14, and year-1 which is only used for calculation.
+    var dayPairs = findPairs(monthNum, weekDay, yearNum);
+    return dayPairs;
+}
+
+export function monDisplay() {
+    var arr = getDaysArray();
+    return <>
+        <h5>hello</h5><h5>hello</h5
+    ></>;
+}
+
+
